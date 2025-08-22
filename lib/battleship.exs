fecc7dc8a4aee328fb4b8ff255839dbc791427a7
seevee <seevee@pm.me>
2025-08-21 18:26:30 -0600

    feat: fog overlay [WIP]

--- lib/battleship.exs
+++ lib/battleship.exs
@@ -20,14 +20,36 @@ defmodule Battleship do
     Integer.mod(length(state.moves), 2) + 1
   end
 
+  def fog_overlay(moves, n) do
+    Enum.map(0..(n - 1), fn y ->
+      Enum.map(0..(n - 1), fn x ->
+        if Enum.member?(moves, {y, x}) do
+          "X"
+        else
+          "~"
+        end
+      end)
+    end)
+  end
+
   def move_overlay(state) do
+    # p1 and p2 alternate for both players
     {p1, p2} = {Enum.take_every(state.moves, 2), Enum.drop_every(state.moves, 2)}
+    IO.puts(inspect(p1))
+    IO.puts(inspect(p2))
 
     case active_player(state) do
       1 ->
+        overlay = fog_overlay(p2, 10)
+
+        IO.puts(inspect(overlay))
+        # draw fog for enemy, hits and misses for enemy and self
         nil
 
       2 ->
+        overlay = fog_overlay(p2, 10)
+
+        IO.puts(inspect(overlay))
         nil
     end
   end
@@ -40,23 +62,20 @@ defmodule Battleship do
     input = String.upcase(IO.gets("Player " <> to_string(active_player(state)) <> " move: "))
 
     # Input validation
-    case String.match?(input, ~r/^([A-J]+\s*\(?\)?)\s*([1-9]|10)$/) do
-      true ->
-        IO.puts("Valid move")
+    if String.match?(input, ~r/^([A-J]+\s*\(?\)?)\s*([1-9]|10)$/) do
+      {row, col} = String.split_at(input, 1)
 
-        {row, col} = String.split_at(input, 1)
+      {y, x} =
+        {:binary.first(row) - 65, String.to_integer(String.trim(col)) - 1}
 
-        {x, y} =
-          {:binary.first(row) - 65, String.to_integer(String.trim(col)) - 1}
+      state = update_in(state.moves, &[{y, x} | &1])
 
-        state = update_in(state.moves, &[[{x, y}] | &1])
-
-        draw_state(state)
-        process_turn(state)
-
-      false ->
-        IO.puts("Invalid move - Try again")
-        process_turn(state)
+      draw_state(state)
+      move_overlay(state)
+      process_turn(state)
+    else
+      IO.puts("Invalid move - Try again")
+      process_turn(state)
     end
   end
 
